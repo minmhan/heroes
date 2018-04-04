2e22aa2881640a32845e15ed193a994b4e93535f
Min Min <min.han@stradegi.com>
2018-04-04 11:03:39 +0800

    update

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,60 +1,24 @@
 import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/toPromise';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 
 import { Hero } from './hero';
-import { Headers, Http } from '@angular/http';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
-    private heroesUrl = 'api/heroes';
 
-    constructor(private http: Http) {}
+  constructor(private messageService: MessageService) { }
 
+  getHeroes() : Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
 
-    getHeroes(): Promise<Hero[]> {
-        return this.http.get(this.heroesUrl)
-            .toPromise()
-            .then(response => response.json().data as Hero[])
-            .catch(this.handleError);
-    }
-
-    getHero(id: number): Promise<Hero> {
-        const url = `${this.heroesUrl}/${id}`;
-        return this.http.get(url)
-            .toPromise()
-            .then(response => response.json().data as Hero)
-            .catch(this.handleError);
-    }
-
-    private headers = new Headers({ 'Content-Type':'application/json'});
-    update(hero: Hero): Promise<Hero> {
-        const url = `${this.heroesUrl}/${hero.id}`;
-        return this.http
-            .put(url, JSON.stringify(hero), { headers: this.headers })
-            .toPromise()
-            .then(() => hero)
-            .catch(this.handleError);
-    }
-
-    create(name: string): Promise<Hero> {
-    return this.http
-        .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
-        .toPromise()
-        .then(res => res.json().data as Hero)
-        .catch(this.handleError);
-    }
-
-    delete(id: number): Promise<void> {
-        const url = `${this.heroesUrl}/${id}`;
-        return this.http.delete(url, {headers: this.headers})
-            .toPromise()
-            .then(() => null)
-            .catch(this.handleError);
-    }
-
-
-    private handleError(error: any): Promise<any>{
-        console.error('An error occured', error)
-        return Promise.reject(error.message || error);
-    }
+  getHero(id: number): Observable<Hero> {
+    // Todo: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
